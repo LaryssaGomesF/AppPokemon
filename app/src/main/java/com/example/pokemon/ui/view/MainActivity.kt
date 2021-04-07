ea759cb6d0111872f4e8d76325a0d426c38017c9
Laryssa Fagundes <laryssa.fagundes@usemobile.xyz>
2021-04-07 09:13:05 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pokemon/ui/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pokemon.ui.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pokemon.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
